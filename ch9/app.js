1903f8873f595fa9d7104c539b1668d99714d8b8
parkbyunggwon <startartart@naver.com>
2023-01-03 16:46:37 +0900

    FEAT: mailtrap 서비스 테스트

--- ch9/app.js
+++ ch9/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config({ path: '.env' });
 
 const nodemailer = require('./send_email');
+const nodemailerMailtrap = require('./send_email_mailtrap');
 
 const app = express();
 
@@ -13,6 +14,12 @@ app.listen(3000, () => console.log('Server is running...'));
 
 app.post('/api/send', async (req, res) => {
     const result = await nodemailer.send(req.body.param);
+    res.send(result);
+    console.log(result);
+});
+
+app.post('/api/send2', async (req, res) => {
+    const result = await nodemailerMailtrap.sendInbox(req.body.param);
     res.send(result);
     console.log(result);
 });
