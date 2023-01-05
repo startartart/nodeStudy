4784e617de350256fce08127478b56abec1eb78e
parkbyunggwon <startartart@naver.com>
2023-01-05 20:02:51 +0900

    FEAT: 다중 멀티 애플리케이션 관리

---
+++ ch9/ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps : [{
+    name   : "app1",
+    script : "./app.js"
+  }
+  /*
+   , {
+    name  : "app2",
+    script: "./app2.js"
+   },
+    {
+    name  : "app3",
+    script: "./app3.js"
+  */
+]
+}
